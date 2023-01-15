7ad10dae8ea2482134740bc04ddc0794046ba7fd
0xorial <0xorial@gmail.com>
2023-01-15 10:37:42 +0100

    Implemented hover under caret.

--- main.ts
+++ main.ts
@@ -13,7 +13,7 @@ import {
   PluginSettingTab,
   Setting,
   View,
-  WorkspaceLeaf
+  WorkspaceLeaf,
 } from "obsidian";
 
 function isMarkdownFileInfoView(x: unknown): x is MarkdownFileInfo {
@@ -287,7 +287,8 @@ export default class PrivacyGlassesPlugin extends Plugin {
     document.body.removeClass(
       "privacy-glasses-blur-all",
       "privacy-glasses-reveal-on-hover",
-      "privacy-glasses-reveal-all"
+      "privacy-glasses-reveal-all",
+      "privacy-glasses-reveal-under-caret"
     );
     if (this.currentLevel === "hide-all") {
       document.body.classList.add("privacy-glasses-blur-all");
@@ -297,6 +298,9 @@ export default class PrivacyGlassesPlugin extends Plugin {
     }
     if (this.settings.hoverToReveal) {
       document.body.classList.add("privacy-glasses-reveal-on-hover");
+    }
+    if (this.settings.revealUnderCaret) {
+      document.body.classList.add("privacy-glasses-reveal-under-caret");
     }
   }
 
@@ -341,19 +345,19 @@ export default class PrivacyGlassesPlugin extends Plugin {
 
 interface PrivacyGlassesSettings {
   blurOnStartup: Level;
-  privacyGlasses: boolean;
   blurLevel: number;
   blurOnIdleTimeoutSeconds: number;
   hoverToReveal: boolean;
+  revealUnderCaret: boolean;
   privateDirs: string;
 }
 
 const DEFAULT_SETTINGS: PrivacyGlassesSettings = {
   blurOnStartup: "hide-private",
-  privacyGlasses: false,
   blurLevel: 0.3,
   blurOnIdleTimeoutSeconds: -1,
   hoverToReveal: true,
+  revealUnderCaret: false,
   privateDirs: "",
 };
 
@@ -436,12 +440,24 @@ class privacyGlassesSettingTab extends PluginSettingTab {
         });
       });
 
+    new Setting(containerEl)
+      .setName("Reveal Under Caret")
+      .setDesc(
+        "Indicates whether or not to reveal content when caret is on it."
+      )
+      .addToggle((toggle) => {
+        toggle.setValue(this.plugin.settings.revealUnderCaret);
+        toggle.onChange(async (value) => {
+          this.plugin.settings.revealUnderCaret = value;
+          await this.plugin.updateGlobalRevealStyle();
+          await this.plugin.saveSettings();
+        });
+      });
+
     var sliderEl = new Setting(containerEl);
     let sliderElDesc = "Higher is blurrier. Default=60, current=";
     sliderEl
-      .setName(
-        'Blur Level'
-      )
+      .setName("Blur Level")
       .setDesc(sliderElDesc + Math.round(this.plugin.settings.blurLevel * 100))
       // ^ need rounding to not show values like '55.00000000000001'
       .addSlider((slider) =>
@@ -477,6 +493,11 @@ class privacyGlassesSettingTab extends PluginSettingTab {
   }
 }
 
+// https://icon-sets.iconify.design/ph/eye-slash/
 const eyeSlashIcon = `<svg xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid meet" viewBox="0 0 256 256"><path fill="currentColor" d="M53.9 34.6a8 8 0 0 0-11.8 10.8l19.2 21.1C25 88.8 9.4 123.2 8.7 124.8a8.2 8.2 0 0 0 0 6.5c.3.7 8.8 19.5 27.6 38.4c25.1 25 56.8 38.3 91.7 38.3a128.6 128.6 0 0 0 52.1-10.8l22 24.2a8 8 0 0 0 5.9 2.6a8.2 8.2 0 0 0 5.4-2.1a7.9 7.9 0 0 0 .5-11.3Zm47.3 75.9l41.7 45.8A31.6 31.6 0 0 1 128 160a32 32 0 0 1-26.8-49.5ZM128 192c-30.8 0-57.7-11.2-79.9-33.3A128.3 128.3 0 0 1 25 128c4.7-8.8 19.8-33.5 47.3-49.4l18 19.8a48 48 0 0 0 63.6 70l14.7 16.2A112.1 112.1 0 0 1 128 192Zm119.3-60.7c-.4.9-10.5 23.3-33.4 43.8a8.1 8.1 0 0 1-5.3 2a7.6 7.6 0 0 1-5.9-2.7a8 8 0 0 1 .6-11.3A131 131 0 0 0 231 128a130.3 130.3 0 0 0-23.1-30.8C185.7 75.2 158.8 64 128 64a112.9 112.9 0 0 0-19.4 1.6a8.1 8.1 0 0 1-9.2-6.6a8 8 0 0 1 6.6-9.2a132.4 132.4 0 0 1 22-1.8c34.9 0 66.6 13.3 91.7 38.3c18.8 18.9 27.3 37.7 27.6 38.5a8.2 8.2 0 0 1 0 6.5ZM134 96.6a8 8 0 0 1 3-15.8a48.3 48.3 0 0 1 38.8 42.7a8 8 0 0 1-7.2 8.7h-.8a7.9 7.9 0 0 1-7.9-7.2A32.2 32.2 0 0 0 134 96.6Z"/></svg>`;
+
+// https://icon-sets.iconify.design/ph/eye-closed-bold/
 const eyeClosedIcon = `<svg xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid meet" viewBox="0 0 256 256"><path fill="currentColor" d="M234.4 160.8a12 12 0 0 1-10.4 18a11.8 11.8 0 0 1-10.4-6l-16.3-28.2a126 126 0 0 1-29.4 13.5l5.2 29.4a11.9 11.9 0 0 1-9.7 13.9l-2.1.2a12 12 0 0 1-11.8-9.9l-5.1-28.7a123.5 123.5 0 0 1-16.4 1a146.3 146.3 0 0 1-16.5-1l-5.1 28.7a12 12 0 0 1-11.8 9.9l-2.1-.2a11.9 11.9 0 0 1-9.7-13.9l5.2-29.4a125.3 125.3 0 0 1-29.3-13.5L42.3 173a12.1 12.1 0 0 1-10.4 6a11.7 11.7 0 0 1-6-1.6a12 12 0 0 1-4.4-16.4l17.9-31a142.4 142.4 0 0 1-16.7-17.6a12 12 0 1 1 18.6-15.1C57.1 116.8 84.9 140 128 140s70.9-23.2 86.7-42.7a12 12 0 1 1 18.6 15.1a150.3 150.3 0 0 1-16.7 17.7Z"/></svg>`;
+
+// https://icon-sets.iconify.design/ph/eye/
 const eyeIcon = `<svg xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid meet" viewBox="0 0 256 256"><path fill="currentColor" d="M247.3 124.8c-.3-.8-8.8-19.6-27.6-38.5C194.6 61.3 162.9 48 128 48S61.4 61.3 36.3 86.3C17.5 105.2 9 124 8.7 124.8a7.9 7.9 0 0 0 0 6.4c.3.8 8.8 19.6 27.6 38.5c25.1 25 56.8 38.3 91.7 38.3s66.6-13.3 91.7-38.3c18.8-18.9 27.3-37.7 27.6-38.5a7.9 7.9 0 0 0 0-6.4ZM128 192c-30.8 0-57.7-11.2-79.9-33.3A130.3 130.3 0 0 1 25 128a130.3 130.3 0 0 1 23.1-30.8C70.3 75.2 97.2 64 128 64s57.7 11.2 79.9 33.2A130.3 130.3 0 0 1 231 128c-7.2 13.5-38.6 64-103 64Zm0-112a48 48 0 1 0 48 48a48 48 0 0 0-48-48Zm0 80a32 32 0 1 1 32-32a32.1 32.1 0 0 1-32 32Z"/></svg>`;
